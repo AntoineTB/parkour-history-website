a43809b26072f64d1e6802c30f065b1de935732c
Antoine Theriault-Brunet <antoine.theriault-brunet@auth0.com>
2022-10-05 01:42:49 -0400

    job attempt
    Signed-off-by: Antoine Theriault-Brunet <antoine.theriault-brunet@auth0.com>

---
+++ jobs/snapshot/index.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+import knex from "knex";
+import { writeFile } from "fs/promises";
+
+const db = knex({
+  client: "pg",
+  connection: process.env.PG_CONNECTION_URL,
+  // searchPath: ['knex', 'public'],
+  pool: { min: 0, max: 2 },
+});
+
+try {
+  const rows = await db.raw(
+    'SELECT sum(1) AS "archivedVideosCount" FROM video WHERE archived = TRUE'
+  );
+  const archivedVideosCount = rows.rows[0].archivedVideosCount;
+  const content = JSON.stringify({
+    archivedVideosCount,
+  });
+  await writeFile("../../docs/_data/snapshot.json", content);
+} catch (error) {
+  console.error(error);
+} finally {
+  db?.destroy();
+}
